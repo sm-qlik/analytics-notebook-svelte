ad9a266ad93162998c324384b6c8110a04fbe3ec
Scott Morrison <scott.morrison@qlik.com>
2025-11-24 11:17:57 -0700

    update config to fix base error

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,30 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import process from 'node:process';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+    // for more information about preprocessors
+    preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter({
+            // Set the output directory for the final static files
+            pages: 'build', // or 'dist', 'docs', etc.
+            assets: 'build', // must match 'pages'
+            fallback: 'index.html', // Optional: creates a fallback 404 page
+            precompress: false,
+            strict: true
+        }),
+        paths: {
+            base: dev ? '' : '/analytics-notebook-svelte',
+        },
+        // Ensures files with underscores (like _app) are served correctly
+        appDir: 'app',
+    }
 };
 
 export default config;
