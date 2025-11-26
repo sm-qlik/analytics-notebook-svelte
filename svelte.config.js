2dc14b3b2230e7c1966d402ba9540d356046b37f
Scott Morrison <scott.morrison@qlik.com>
2025-11-26 09:21:35 -0700

    updated config for netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,30 +1,18 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import process from 'node:process';
-
-const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-    // for more information about preprocessors
-    preprocess: vitePreprocess(),
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
 
-    kit: {
-        adapter: adapter({
-            // Set the output directory for the final static files
-            pages: 'build', // or 'dist', 'docs', etc.
-            assets: 'build', // must match 'pages'
-            fallback: 'index.html', // Optional: creates a fallback 404 page
-            precompress: false,
-            strict: true
-        }),
-        paths: {
-            base: dev ? '' : '/analytics-notebook-svelte',
-        },
-        // Ensures files with underscores (like _app) are served correctly
-        appDir: 'app',
-    }
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
+	}
 };
 
 export default config;
