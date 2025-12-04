5ae5549ecaca01ca3e86a37b7765ca502cc855a3
Scott Morrison <scott.morrison@qlik.com>
2025-12-04 09:33:08 -0700

    updated to static build for github pages

--- build/app/immutable/entry/start.55AEOpDr.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/CimRaeZi.js";import{w as t}from"../chunks/DNTpd9p3.js";export{t as load_css,r as start};
--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,24 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html', // Enable SPA mode for dynamic routes (OAuth callbacks, etc.)
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: dev ? '' : '/analytics-notebook-svelte', // Replace 'your-repo-name' with your repository name
+		}
 	}
 };
 
