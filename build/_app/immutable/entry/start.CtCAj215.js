79b0ef98ef04999f14fa4be888dcfaa920b7d957
Scott Morrison <scott.morrison@qlik.com>
2025-12-04 21:59:14 -0700

    fix for loading app data

---
+++ build/_app/immutable/entry/start.CtCAj215.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/CTi1OoMO.js";import{v as t}from"../chunks/Bw0PGpte.js";export{t as load_css,r as start};
--- build/_app/immutable/entry/start.DQmRcnmB.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/CW5uYo4y.js";import{v as t}from"../chunks/BzaIsxho.js";export{t as load_css,r as start};
